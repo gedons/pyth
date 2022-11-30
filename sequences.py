4a1a8525ba913d0fff1c6fa14300edadc6891d59
Nerdy <gedoniani@gmail.com>
2022-11-30 15:39:12 +0100

    sequence overview

---
+++ sequences.py
@@ -0,0 +1,14 @@
+#String
+name = "Harry"
+print(name[4])
+
+#Lists
+names = ["harry", "Ronnie", "Hannah"]
+print(names[0])
+
+#Turples
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+ 
